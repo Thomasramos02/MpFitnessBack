9743b431c3558523d946f5407a38409be0dc2c61
Thomas Ramos Oliveira <161534687+Thomasramos02@users.noreply.github.com>
2025-08-29 23:29:50 -0300

    Delete MpFitnessBack directory

--- MpFitnessBack/src/main/java/com/example/MpFitness/DTO/NovaSenhaRequest.java
+++
@@ -1,7 +0,0 @@
-package com.example.MpFitness.DTO;
-
-public class NovaSenhaRequest {
-    private String novaSenha;
-    public String getNovaSenha() { return novaSenha; }
-    public void setNovaSenha(String novaSenha) { this.novaSenha = novaSenha; }
-}
